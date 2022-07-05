b6bc5a9b0f62f4d5d264d79be6603d49b1fab501
grez72 <grez72@gmail.com>
2022-07-05 15:10:18 -0400

    grr

--- hubconf.py
+++ hubconf.py
@@ -2,9 +2,7 @@ import os
 import torch
 import torchvision
 
-from models import AlexNetRotNet
-from models.alexnet_deepcluster import AlexnetDeepCluster
-from models.vgg16_deepcluster import VGG16DeepCluster
+from models import AlexNetRotNet, AlexnetDeepCluster, VGG16DeepCluster
 
 from torchvision.models import resnet50
 
